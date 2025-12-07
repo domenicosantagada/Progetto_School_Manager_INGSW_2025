d181241a476194b15360277e699a42b20b7e9330
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2025-12-07 22:07:05 +0100

    Commentata la classe LoginController

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -17,33 +17,36 @@ public class LoginController {
 
     @FXML
     private ImageView logoView;
-
     @FXML
     private TextField userField;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private TextField passwordVisibleField;
-
     @FXML
     private FontAwesomeIcon toggleIconPassword;
 
+    // Metodo per navigare alla pagina di registrazione
     @FXML
     private void registrationClicked() throws IOException {
         SceneHandler.getInstance().setRegistrationPage();
     }
 
+    // Metodo per effettuare il login
     @FXML
     private void loginClicked() throws IOException {
         String username = userField.getText();
         String password = passwordField.getText();
 
 
+        // Controllo che i campi non siano vuoti
         if (username.isEmpty() || password.isEmpty()) {
             SceneHandler.getInstance().showWarning(MessageDebug.CAMPS_NOT_EMPTY);
-        } else if (Database.getInstance().validateCredentials(username, password)) {
+        }
+        // Controllo che le credenziali siano valide
+        else if (Database.getInstance().validateCredentials(username, password)) {
+
+            // Naviga alla home page in base al tipo di utente
             String typeUser = Database.getInstance().getTypeUser(username);
             if (typeUser.equals("studente")) {
                 SceneHandler.getInstance().setStudentHomePage(username);
@@ -51,6 +54,7 @@ public class LoginController {
                 SceneHandler.getInstance().setProfessorHomePage(username);
             }
         } else {
+            // Mostro un messaggio di errore se le credenziali non sono valide
             SceneHandler.getInstance().showWarning(MessageDebug.CREEDENTIALS_NOT_VALID);
         }
     }
@@ -58,12 +62,15 @@ public class LoginController {
     public void initialize() {
         String imagePath = getClass().getResource("/icon/logo.png").toExternalForm();
         logoView.setImage(new Image(imagePath));
+
+        // aggiungiamo i listener per la sincronizzazione dei campi password
         addListeners();
 
         // settiamo il focus sul campo username
         userField.requestFocus();
     }
 
+    // Metodo per aggiungere i listener ai campi password
     private void addListeners() {
         passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!passwordVisibleField.isFocused()) {
@@ -92,6 +99,8 @@ public class LoginController {
 
     }
 
+    // Metodo per mostrare/nascondere la password
+    @FXML
     public void togglePasswordVisibility(MouseEvent mouseEvent) {
         if (passwordField.isVisible()) {
             passwordField.setVisible(false);
