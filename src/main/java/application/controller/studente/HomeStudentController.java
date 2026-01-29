0b0f00098630de96eeff00e6215e8f13f4921abc
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-01-29 17:30:44 +0100

    Rilettura del codice e modifiche: HomeStudente

--- src/main/java/application/controller/studente/HomeStudentController.java
+++ src/main/java/application/controller/studente/HomeStudentController.java
@@ -19,49 +19,72 @@ public class HomeStudentController {
     @FXML
     private Label classeStudente;     // Classe dello studente
 
+
+    private SceneHandler sh;
+    private Database db;
+    private String usernameStudente, classe;
+
+
+    // Inizializza la home dello studente: carica logo, nome e classe
+    public void initialize() {
+
+        sh = SceneHandler.getInstance();
+        db = Database.getInstance();
+
+        usernameStudente = sh.getUsername();
+        classe = db.getClasseUser(usernameStudente);
+
+        caricaLogo();
+        caricaInfoStudente();
+
+
+    }
+
+    private void caricaLogo() {
+        // Carica il logo dell'istituto
+        String imagePath = getClass().getResource("/icon/logo1.png").toExternalForm();
+        logoView.setImage(new Image(imagePath));
+        logoView.setSmooth(true);
+    }
+
+    private void caricaInfoStudente() {
+        // Imposta il nome completo dello studente
+        String studentInfoText = db.getFullName(usernameStudente);
+        studentInfo.setText(studentInfoText.toUpperCase());
+
+        // Imposta la classe dello studente
+        classeStudente.setText(classe.toUpperCase());
+    }
+
+
     // Mostra la pagina delle performance dello studente
     @FXML
     public void performanceClicked(ActionEvent actionEvent) throws IOException {
-        SceneHandler.getInstance().setAndamentoPage();
+        sh.setAndamentoPage();
     }
 
     // Mostra la pagina dei compiti assegnati
     @FXML
     public void assignmentClicked(ActionEvent actionEvent) throws IOException {
-        SceneHandler.getInstance().setCompitiPage();
+        sh.setCompitiPage();
     }
 
     // Mostra la pagina delle note disciplinari
     @FXML
     public void notesButtonClicked(ActionEvent actionEvent) throws IOException {
-        SceneHandler.getInstance().setNotePage();
+        sh.setNotePage();
     }
 
     // Mostra la pagina delle assenze dello studente
     @FXML
     public void assenzeClicked(ActionEvent actionEvent) throws IOException {
-        SceneHandler.getInstance().setAssenzeStudentePage();
+        sh.setAssenzeStudentePage();
     }
 
     // Effettua il logout e ritorna alla pagina di login
     @FXML
     public void logoutClicked(ActionEvent actionEvent) throws IOException {
-        SceneHandler.getInstance().setLoginPage();
+        sh.setLoginPage();
     }
-
-    // Inizializza la home dello studente: carica logo, nome e classe
-    public void initialize() {
-        // Carica il logo dell'istituto
-        String imagePath = getClass().getResource("/icon/logo1.png").toExternalForm();
-        logoView.setImage(new Image(imagePath));
-        logoView.setSmooth(true);
-
-        // Imposta il nome completo dello studente
-        String studentInfoText = Database.getInstance().getFullName(SceneHandler.getInstance().getUsername());
-        studentInfo.setText(studentInfoText.toUpperCase());
-
-        // Imposta la classe dello studente
-        String classe = Database.getInstance().getClasseUser(SceneHandler.getInstance().getUsername());
-        classeStudente.setText(classe.toUpperCase());
-    }
+    
 }
