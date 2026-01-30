894bc8c79c3483597af1a132b1988885ae7753a1
Domenico Santagada <115139470+domenicosantagada@users.noreply.github.com>
2026-01-30 09:46:47 +0100

    Rilettura del codice e modifiche

---
+++ src/main/java/application/MainTest.java
@@ -0,0 +1,13 @@
+package application;
+
+import application.persistence.Database;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Database db = Database.getInstance();
+
+        System.out.println(db.getAllMaterieIstituto());
+
+
+    }
+}
